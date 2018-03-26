b3080009f44b326905d686601da78ee0ad258dd8
Megan Dotto <megan.dotto@gmail.com>
2018-03-26 17:55:12 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-    i = 0
-    while i < collection.length
-      yield(collection[i])
-      i = i + 1
-    end
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+  end
 end
 
 collection = (1..100).to_a
